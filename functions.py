32a0ed5a1b4bbebb002d667af22d0a2cf2e40dc9
Coralis967 <coreyscoding@gmail.com>
2022-12-22 20:41:08 +1000

    added progress counter and ignore test file

--- functions.py
+++ functions.py
@@ -13,12 +13,16 @@ def translate_line(line):
 #TODO maybe add a progress counter.
 def translate_file_to_list(language_file):
     translated_list = []
+    total_lines = count_lines(language_file)
     with io.open(language_file, 'r', encoding = 'utf-8') as old_file:
         line = old_file.readline()
+        count = 0
         while line:
             try:
                 translated_list.append(translate_line(line))
                 line = old_file.readline()
+                print(f"Current Line: {count}/{total_lines}")
+                count += 1
             except UnicodeDecodeError:
                 return "UnicodeDecodeError, check the input text encoding."
     return translated_list
@@ -32,4 +36,9 @@ def request_language_file():
 def output_new_file(translated_list):
     pass
 
-            
+def count_lines(file):
+    with io.open(file, 'r', encoding = 'utf-8') as old_file:
+        total_lines = len(old_file.readlines())
+    return total_lines
+
+
